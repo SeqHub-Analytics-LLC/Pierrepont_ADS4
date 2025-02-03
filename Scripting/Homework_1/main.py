56038cb8c8b71a47e1212593f6464069ebc33256
Simon Meyers <simonmeyers@Simons-MacBook-Air.local>
2025-02-03 14:07:45 -0500

    Updated Scripts, downloaded Chapter 2 Notebook, and reconfigured my branch

---
+++ Scripting/Homework_1/main.py
@@ -0,0 +1,10 @@
+from data_processing import load_data
+from model_training import train_decision_tree, train_random_forest, train_gradient_boosting,train_xgboost_with_optuna
+from model_validation import evaluate
+
+def main():
+    
+
+
+if __name__ == "__main__":
+    main()
--- Scripting/Homework_1/model_training.py
+++ Scripting/Homework_1/model_training.py
@@ -1,31 +1,25 @@
-from sklearn.metrics import mean_squared_error, r2_score
+def train_decision_tree(X_train, y_train):
+    model = DecisionTreeRegressor(random_state=23, max_depth=7)
+    model.fit(X_train, y_train)
+    return model
 
-def evaluate(train_predictions, test_predictions):
+def train_random_forest(X_train, y_train):
+    model = RandomForestRegressor(random_state=34, n_estimators=500, min_samples_leaf=40, min_samples_split=90, n_jobs=-1, max_features="sqrt")
+    model.fit(X_train, y_train)
+    return model
 
-    r2_train = r2_score(train_predictions, y_train)
-    r2_test = r2_score(test_predictions, y_test)
-    print(f"R2:\nTrain: {r2_train}, Test: {r2_test}")
+def train_gradient_boosting_tree(X_train, y_train):
+    model = GradientBoostingRegressor(max_depth=3, random_state=85)
+    model.fit(X_train, y_train)
+    return model
 
-    rmse_train = np.sqrt(mean_squared_error(train_predictions, y_train))
-    rmse_test = np.sqrt(mean_squared_error(test_predictions, y_test))
-    print(f"RMSE:\nTrain: {rmse_train}, Test: {rmse_test}")
-
-from matplotlib import pyplot as plt
-def plot_feature_importance(importance, names, model_name):
-    #create arrays from feature importance and feature names
-    feature_importance = np.array(importance)
-    feature_names = np.array(names)
-
-    # create a dataframe using a dictionary
-    data = {'feature_names':feature_names, 'feature_importance':feature_importance}
-    fi_df = pd.DataFrame(data)
-    fi_df.sort_values(by=['feature_importance'],ascending = False, inplace=True)
-
-    #define size of bar plot
-    plt.figure(figsize=(10,8))
-    sns.barplot(x=fi_df['feature_importance'], y = fi_df['feature_names'])
-    plt.title(model_name + "FEATURE IMPORTANCE")
-
-    #Add chart labels
-    plt.xlabel("FEATURE IMPORTANCE")
-    plt.ylabel("FEATURE NAMES")
+def objective(trial, X_train, y_train, X_val, y_val):
+    params = {
+        'n_estimators': trial.suggest_int('n_estimators', 50, 500),
+        'max_depth': trial.suggest_int('max_depth', 3, 10),
+        'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.3),
+        'subsample': trial.suggest_float('subsample', 0.5, 1.0),
+        'colsample_bytree': trial.suggest_float('colsample_bytree', 0.5, 1.0),
+        'reg_alpha': trial.suggest_float('reg_alpha', 0.0, 1.0),
+        'reg_lambda': trial.suggest_float('reg_lambda', 0.0, 1.0)
+    }
